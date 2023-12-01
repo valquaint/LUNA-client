d4d5231a6deb16b3d1d71b168a8b7e1fda850890
Valquaint <valquaint@gmail.com>
2023-12-01 14:32:25 -0800

    Minor cleanup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,25 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 1000,
+      backgroundColor: "#ff9900ff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
-import process from 'node:process';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 // Call the element loader before the render call
 defineCustomElements(window);
 
-  const container = document.getElementById('root');
-  const root = createRoot(container!);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+const container = document.getElementById('root');
+const root = createRoot(container!);
+async function init() {
+  console.log("Showing splash...")
+  await SplashScreen.show({
+    showDuration:2000,
+    autoHide: true,
+  });
+}
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+init();
